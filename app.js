489562737ca2b037b2f6f9e4d6c665567de13aab
NemesisUA <display.n0ne.01.gmail.com>
2023-05-03 20:19:33 +0300

    feat: connect mongoDB, create model

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 const app = express();
 
-app.listen(3000);
+// connect to mongodb & listen for requests
+const dbURI = 'mongodb+srv://testuser:test123@blog1.pvdtv7x.mongodb.net/blog1';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(app.listen(3000))
+  .catch(error => console.log(error));
 
 app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
-app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
-  res.render('index', {title: 'Home', blogs});
+app.get('/', (req, res) => {  
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
 
-//redirect 301
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+// blogs
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1})
+    .then(result => {
+      res.render('index', { title: 'Blogs', blogs: result})
+    })
+    .catch(err => console.log(err));
 })
+
 
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
