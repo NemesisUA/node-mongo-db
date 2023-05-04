7fd270023be1dde7fd085d15be7a74aee11dd771
NemesisUA <display.n0ne.01.gmail.com>
2023-05-04 21:01:35 +0300

    feat: add POST request

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+
 app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
@@ -37,6 +39,13 @@ app.get('/blogs', (req, res) => {
     .catch(err => console.log(err));
 })
 
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog.save()
+    .then(result => res.redirect('/blogs') )
+    .catch(err => console.log(err));
+})
 
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
